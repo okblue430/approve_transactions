49633b6b1ef36b2df0fdc8ab78124194c33c6b94
okblue <kennylee991323@gmail.com>
2023-03-29 14:23:23 -0500

    first commit

---
+++ src/components/Transactions/TransactionPane.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import { InputCheckbox } from "../InputCheckbox"
+import { TransactionPaneComponent } from "./types"
+
+export const TransactionPane: TransactionPaneComponent = ({
+  transaction,
+  loading,
+  setTransactionApproval: consumerSetTransactionApproval,
+}) => {
+  const [approved, setApproved] = useState(transaction.approved)
+
+  const onClick = async () => {
+    let newValue = !transaction.approved;
+    await consumerSetTransactionApproval({
+      transactionId: transaction.id,
+      newValue,
+    })
+
+    setApproved(newValue)
+  }
+
+  return (
+    <div className="RampPane">
+      <div className="RampPane--content">
+        <p className="RampText">{transaction.merchant} </p>
+        <b>{moneyFormatter.format(transaction.amount)}</b>
+        <p className="RampText--hushed RampText--s">
+          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+        </p>
+      </div>
+      <InputCheckbox
+        id={transaction.id}
+        checked={approved}
+        disabled={loading}
+        onChange={async (newValue) => {
+          await consumerSetTransactionApproval({
+            transactionId: transaction.id,
+            newValue,
+          })
+
+          setApproved(newValue)
+        }}
+        onClick={onClick}
+      />
+    </div>
+  )
+}
+
+const moneyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})
